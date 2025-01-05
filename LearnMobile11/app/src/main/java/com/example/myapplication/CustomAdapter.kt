6f20768f17fefc8f995282f478c2a27085e3e1ed
LongLee239142 <long.ld195887@gmail.com>
2025-01-05 16:46:20 +0700

    update

--- LearnMobile11/app/src/main/java/com/example/myapplication/CustomAdapter.kt
+++ LearnMobile11/app/src/main/java/com/example/myapplication/CustomAdapter.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class CustomAdapter(val activity: MainActivity, val list: List<OutData>) :
     ArrayAdapter<OutData>(activity, R.layout.list_item) {
@@ -11,7 +13,14 @@ class CustomAdapter(val activity: MainActivity, val list: List<OutData>) :
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-         val contex = activity.layoutInflater //
-        return super.getView(position, convertView, parent)
+         val contexs = activity.layoutInflater // layoutInflater la 1 component giup chuyen file xml -> view trong android
+        val rowView = contexs.inflate(R.layout.list_item,parent,false)
+        val image = rowView.findViewById<ImageView>(R.id.images)
+        val title = rowView.findViewById<TextView>(R.id.title)
+        val desc = rowView.findViewById<TextView>(R.id.description)
+        title.text = list[position].title
+        desc.text = list[position].description
+        image.setImageResource(list[position].image)
+        return rowView
     }
 }
--- LearnMobile11/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ LearnMobile11/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.databinding.ActivityMainBinding
 
-@SuppressLint("StaticFieldLeak")
-private lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    lateinit var customAdapter: CustomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +20,43 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //khai bao danh sach item list
+        var list = mutableListOf<OutData>()
+        list.add(OutData(R.drawable.image1, "Chi Dau", " This is a description of Chi Dau"))
+        list.add(OutData(R.drawable.image2, "Run Away", " This is a description of Run Away"))
+        list.add(
+            OutData(
+                R.drawable.image3,
+                "Quy Hoi Quan",
+                " This is a description of Quy Hoi Quan"
+            )
+        )
+        list.add(OutData(R.drawable.image4, "Lion King", " This is a description of Lion King"))
+        list.add(OutData(R.drawable.image5, "Kính Vạn Hoa", " This is a description of Kính Vạn Hoa"))
+        list.add(OutData(R.drawable.image6, "Sonic 3", " This is a description of Sonic 3"))
+        list.add(OutData(R.drawable.image7, "Mưa trên cách bướm", " This is a description of Mưa trên cánh Bướm"))
+        list.add(OutData(R.drawable.image4, "Lion King", " This is a description of Lion King"))
+        list.add(OutData(R.drawable.image5, "Kính Vạn Hoa", " This is a description of Kính Vạn Hoa"))
+        list.add(OutData(R.drawable.image6, "Sonic 3", " This is a description of Sonic 3"))
+        list.add(OutData(R.drawable.image7, "Mưa trên cách bướm", " This is a description of Mưa trên cánh Bướm"))
+        list.add(OutData(R.drawable.image4, "Lion King", " This is a description of Lion King"))
+        list.add(OutData(R.drawable.image5, "Kính Vạn Hoa", " This is a description of Kính Vạn Hoa"))
+        list.add(OutData(R.drawable.image6, "Sonic 3", " This is a description of Sonic 3"))
+        list.add(OutData(R.drawable.image7, "Mưa trên cách bướm", " This is a description of Mưa trên cánh Bướm"))
+        list.add(OutData(R.drawable.image4, "Lion King", " This is a description of Lion King"))
+        list.add(OutData(R.drawable.image5, "Kính Vạn Hoa", " This is a description of Kính Vạn Hoa"))
+        list.add(OutData(R.drawable.image6, "Sonic 3", " This is a description of Sonic 3"))
+        list.add(OutData(R.drawable.image7, "Mưa trên cách bướm", " This is a description of Mưa trên cánh Bướm"))
+
+        customAdapter = CustomAdapter(this, list)
+        //khai báo 1 biến  link tới listMovie
+        val listMovie = findViewById<ListView>(R.id.listMovie)
+        listMovie.adapter = customAdapter
+
+        // listview item click listener
+        listMovie.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "You have to choose " + list[position].title,Toast.LENGTH_SHORT).show()
         }
     }
 }
